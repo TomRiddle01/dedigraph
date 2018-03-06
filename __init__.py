21e90a98f481d1290fb54b47e5cf271f778d39a9
Yannis Rohloff <yannis@uni-bremen.de>
2018-03-06 23:19:48 +0100

    initial

---
+++ __init__.py
@@ -0,0 +1,139 @@
+from pyplanet.apps.config import AppConfig
+from pyplanet.apps.core.trackmania import callbacks as tm_signals
+from pyplanet.apps.core.maniaplanet import callbacks as mp_signals
+from pyplanet.contrib.command import Command
+
+from .view import GraphView
+
+
+class DediGraphApp(AppConfig):
+    game_dependencies = ['trackmania', 'shootmania']
+    mode_dependencies = ['TimeAttack']
+    app_dependencies = ['core.maniaplanet']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.widget = None
+        self.local_records = None
+        self.dedimania = None
+
+    async def on_init(self):
+        await super().on_init()
+
+    async def on_stop(self):
+        await super().on_stop()
+
+    async def on_destroy(self):
+        await super().on_destroy()
+
+    async def on_start(self):
+        await super().on_start()
+
+        self.context.signals.listen(mp_signals.map.map_start, self.reset_widget)
+        self.context.signals.listen(tm_signals.start_countdown, self.display_graph)
+
+        await self.instance.command_manager.register(
+            Command(command='compare', target=self.compare).add_param('playerlogin', nargs='1', type=str, required=True, help='Playerlogin to compare to. (Will be plotted in blue.)'),
+        )
+
+        self.widget = GraphView(self)
+        self.local_records = self.instance.apps.apps["local_records"]
+
+        if "dedimania" not in self.instance.apps.apps:
+
+            class StubDedi():
+                def __init__(self):
+                    self.current_records = []
+
+            class StubRecord():
+                def __init__(self, cps, player):
+                    self.cps = cps
+                    self.login = player
+
+            self.dedimania = StubDedi()
+            # self.dedimania.current_records.append(StubRecord("1000,2300,2800,3000", "tomriddle"))
+            # self.dedimania.current_records.append(StubRecord("1300,2400,2800,3500", "steven"))
+            # self.dedimania.current_records.append(StubRecord("1100,2300,2400,3700", "peavis"))
+        else:
+            self.dedimania = self.instance.apps.apps["dedimania"]
+
+    async def compare(self, player, data, **kwargs):
+        await self.load_selected_player_graph(data.playerlogin, player)
+        await self.instance.chat("You are now comparing to "+data.playerlogin+" (displayed in blue).", player.login)
+        await self.display_graph(player)
+
+
+    async def reset_widget(self, **kwargs):
+        await self.widget.reset()
+
+    async def display_graph(self, player, **kwargs):
+        await self.load_dedi_graph(player)
+        await self.load_local_graph(player)
+        await self.display_own_records_graph(player)
+        await self.widget.display(player=player.login)
+
+    async def load_selected_player_graph(self, selected_player, player, **kwagrs):
+        for record in self.dedimania.current_records:
+            if record.login == selected_player:
+                cps = []
+                if type(record.cps) is str:
+                    cps = record.cps.split(",")
+                elif type(record.cps) is list:
+                    cps = record.cps
+
+                await self.widget.update_times("other_dedi", player, cps)
+
+        for record in self.local_records.current_records:
+            if record.player.login == selected_player:
+                cps = []
+                if type(record.checkpoints) is str:
+                    cps = record.checkpoints.split(",")
+                elif type(record.checkpoints) is list:
+                    cps = record.checkpoints
+                await self.widget.update_times("other_local", player, cps)
+
+    async def display_own_records_graph(self, player, **kwagrs):
+        for record in self.dedimania.current_records:
+            if record.login == player.login:
+                cps = []
+                if type(record.cps) is str:
+                    cps = record.cps.split(",")
+                elif type(record.cps) is list:
+                    cps = record.cps
+
+                await self.widget.update_times("own_dedi", player, cps)
+        
+        for record in self.local_records.current_records:
+            if record.player.login == player.login:
+                cps = []
+                if type(record.checkpoints) is str:
+                    cps = record.checkpoints.split(",")
+                elif type(record.checkpoints) is list:
+                    cps = record.checkpoints
+                await self.widget.update_times("own_local", player, cps)
+
+
+    async def load_local_graph(self, player, **kwagrs):
+        num_records = len(self.local_records.current_records)
+
+        if num_records > 0:
+            record = self.local_records.current_records[0]
+            if type(record.checkpoints) is str:
+                cps = record.checkpoints.split(",")
+                await self.widget.update_times("dedi_first", player, cps)
+            elif type(record.checkpoints) is list:
+                cps = record.checkpoints
+                await self.widget.update_times("dedi_first", player, cps)
+
+    async def load_dedi_graph(self, player, **kwagrs):
+        num_records = len(self.dedimania.current_records)
+
+        if num_records > 0:
+            record = self.dedimania.current_records[0]
+            if type(record.cps) is str:
+                cps = record.cps.split(",")
+                await self.widget.update_times("dedi_first", player, cps)
+            elif type(record.cps) is list:
+                cps = record.cps
+                await self.widget.update_times("dedi_first", player, cps)
+
---
+++ view.py
@@ -0,0 +1,53 @@
+from pyplanet.views.generics.widget import WidgetView
+
+
+class GraphView(WidgetView):
+    
+    # under dedi
+    # widget_x = -158
+    # widget_y = -65
+    
+    # over dedi
+    # widget_x = -160
+    # widget_y = 15
+
+    # head
+    widget_x = -124
+    widget_y = 65
+
+    template_name = 'dedigraph/graph.xml'
+
+    def __init__(self, app):
+        super().__init__(self)
+        self.app = app
+        self.manager = app.context.ui
+        self.id = 'pyplanet__widgets_graph'
+        self.player_data = {}
+        self.checkpoints = 0
+
+    def create_if_not_exists(self, player):
+        if player.login not in self.player_data:
+            self.player_data[player.login] = {"cptimes": {}}
+        
+    async def reset(self):
+        self.player_data = {}
+        self.checkpoints = 0
+
+    async def update_times(self, type, player, times):
+        self.create_if_not_exists(player)
+        self.player_data[player.login]["cptimes"][type] = ",".join(map(str, map(float, times)))
+
+    async def get_context_data(self):
+        context = await super().get_context_data()
+
+        # Add facts.
+        context.update({
+            'num_checkpoints': self.app.instance.map_manager.current_map.num_checkpoints,
+        })
+
+        return context
+
+    async def get_player_data(self):
+        data = await super().get_player_data()
+        data.update(self.player_data)
+        return data
